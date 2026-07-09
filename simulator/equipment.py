7a3b889e0bf37a9a38cbd35d3d584b020d54877f
blueoce006-eng <blueoce006@gmail.com>
2026-07-09 16:05:57 +0900

    Update equipment.py

--- simulator/equipment.py
+++ simulator/equipment.py
@@ -1,1 +1,24 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Equipment:
+    """
+    Represents equipment health.
+    """
+
+    health: float = 1.0
+    contamination: float = 0.0
+
+    def age(self, amount: float = 0.0001):
+        """
+        Equipment slowly degrades.
+        """
+        self.health = max(0.8, self.health - amount)
+
+    def clean(self):
+        """
+        Maintenance.
+        """
+        self.contamination = 0.0
+        self.health = 1.0
